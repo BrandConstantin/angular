d61a579a959564531fd25d359a2ce35b93a4e281
BrinduConstantin <constantin.brindu@gmail.com>
2021-03-31 19:31:37 +0200

    combining evet and property binding

--- projectAngular9/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projectAngular9/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipieWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe('A simple recipe', 'Do this in only 15 min', 'https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-1500x1125.jpg'),
     new Recipe('A green recipe', 'You wanna a green salat?!', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?fit=1200,879&ssl=1')
@@ -17,4 +19,8 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  
+  onRecipeSelected(recipeElement: Recipe){
+    this.recipieWasSelected.emit(recipeElement)
+  }
 }
