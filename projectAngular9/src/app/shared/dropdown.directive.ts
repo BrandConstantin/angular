5507a3d7f991d7a1238961bf9b7254afa50ac9de
BrinduConstantin <constantin.brindu@gmail.com>
2021-04-02 23:42:26 +0200

    closing the dropdown from enywhere

--- projectAngular9/src/app/shared/dropdown.directive.ts
+++ projectAngular9/src/app/shared/dropdown.directive.ts
@@ -10,3 +10,12 @@ export class DropdownDirective{
         this.isOpen = !this.isOpen;
     }
 }
+
+// to close the dropdown from enyware
+// export class DropdownDirective {
+//     @HostBinding('class.open') isOpen = false;
+//     @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+//       this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+//     }
+//     constructor(private elRef: ElementRef) {}
+//   }
