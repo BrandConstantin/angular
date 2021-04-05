e0e084ce6b392fe441dbbb3ddb88f65c593ada12
BrinduConstantin <constantin.brindu@gmail.com>
2021-04-05 14:00:29 +0200

    passing data to list

--- projectAngular9/src/app/shopping-list/shopping-list.service.ts
+++ projectAngular9/src/app/shopping-list/shopping-list.service.ts
@@ -16,4 +16,15 @@ export class ShoppingListService{
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice());
     }
+
+    addIngredients(ingredients: Ingredient[]){
+        // can do this
+        // for(let ingredient of ingredients){
+        //     this.addIngredient(ingredient);
+        // }
+
+        // or this
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
