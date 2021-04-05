b92e41de6c73d04d73b18ad0f92912adeb2155ff
BrinduConstantin <constantin.brindu@gmail.com>
2021-04-05 13:44:52 +0200

    services and dependency injection

--- projectAngular9/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projectAngular9/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,26 +1,18 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
-  @Output() recipieWasSelected = new EventEmitter<Recipe>();
-  
-  recipes: Recipe[] = [
-    new Recipe('A simple recipe', 'Do this in only 15 min', 'https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-1500x1125.jpg'),
-    new Recipe('A green recipe', 'You wanna a green salat?!', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?fit=1200,879&ssl=1')
-  ];
+export class RecipeListComponent implements OnInit {  
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-  }
-
-  
-  onRecipeSelected(recipeElement: Recipe){
-    this.recipieWasSelected.emit(recipeElement)
+    this.recipes = this.recipeService.getRecipes();
   }
 }
---
+++ projectAngular9/src/app/recipes/recipe.service.ts
@@ -0,0 +1,33 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { Recipe } from "./recipe.model";
+
+
+export class RecipeService{
+    recipeSelected = new EventEmitter<Recipe>();
+
+    recipes: Recipe[] = [
+        new Recipe(
+            'Barbaque Chicken Wings', 
+            'Do this in only 15 min', 
+            'https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-1500x1125.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Souce', 1)
+            ]),
+        new Recipe(
+            'Pumpkin Pad Thai', 
+            'You wanna a green salat?!', 
+            'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?fit=1200,879&ssl=1',
+            [
+                new Ingredient('Pasta', 1),
+                new Ingredient('Judias', 1),
+                new Ingredient('Arrandanos', 10),
+                new Ingredient('Limon', 1)
+            ])
+      ];
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+}
